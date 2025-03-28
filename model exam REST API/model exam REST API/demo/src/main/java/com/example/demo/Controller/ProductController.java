35e5704d4ea03e65992840bf815be3fd256ed564
AthisayaIsak <727723eucd008@skcet.ac.in>
2025-03-28 09:12:02 +0530

    Add files via upload

---
+++ model exam REST API/model exam REST API/demo/src/main/java/com/example/demo/Controller/ProductController.java
@@ -0,0 +1,36 @@
+package com.example.demo.Controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+import com.example.demo.Model.Product;
+import com.example.demo.Service.ProductService;
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+@RequestMapping("/products")
+public class ProductController {
+
+    @Autowired
+    private ProductService productService;
+
+    @GetMapping
+    public List<Product> getAllProducts() {
+        return productService.getAllProducts();
+    }
+
+    @GetMapping("/{id}")
+    public Optional<Product> getProductById(@PathVariable Long id) {
+        return productService.getProductById(id);
+    }
+
+    @PostMapping
+    public Product createProduct(@RequestBody Product product) {
+        return productService.saveProduct(product);
+    }
+
+    @DeleteMapping("/{id}")
+    public void deleteProduct(@PathVariable Long id) {
+        productService.deleteProduct(id);
+    }
+}
